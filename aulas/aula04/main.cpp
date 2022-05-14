e6cc0fc4db4c9877a89eb889e9155c98821e3b6c
eduardo-developer01 <eduardosilvaprogramador@gmail.com>
2022-05-14 00:02:27 -0300

    aula 04 variaveis

---
+++ aulas/aula04/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int life=10;// 12, 42
+    char letra='A';// 'A' uma letra
+    double decimal=2.4444;// 2,44545435
+    float decima2=5.5;// 2.5
+    bool ok=true; // true or false
+    string name="eduardo"; // eduardo
+
+    cout << life << "\n";
+    cout << letra << "\n";
+    cout << decimal << "\n";
+    cout << decima2 << "\n";
+    cout << ok << "\n";
+    cout << name << "\n";
+
+    return 0;
+}
+
