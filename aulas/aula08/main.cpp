6b5171ed92a44fe1004d6d18eb94805373cd4a27
eduardo-developer01 <eduardosilvaprogramador@gmail.com>
2022-05-14 11:15:51 -0300

    aula 09 invertando valores

---
+++ aulas/aula08/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int number = 10;
+
+    cout << number << "\n";
+
+    number *= -1;
+
+    cout << -number << "\n";
+
+    number -= number;
+
+    cout << number << "\n";
+
+    return 0;
+}
+
