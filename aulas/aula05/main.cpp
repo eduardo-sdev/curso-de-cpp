9d0a7e16015d7a5f31faa1bc764365ffbb990d37
eduardo-developer01 <eduardosilvaprogramador@gmail.com>
2022-05-14 00:02:56 -0300

    aula 05 declaracoes multiplas de variaveis contantes

---
+++ aulas/aula05/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#define pi 3.1415
+#define hello cout << "hello, world\n";
+
+using namespace std;
+
+int main() {
+
+    int vidas=3, tiros=130, life=100;
+    
+    hello
+    cout << pi;
+
+    return 0;
+}
+
