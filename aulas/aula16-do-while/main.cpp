8307784214135d44b26ded6a3428c83deaa629a2
Eduardo Silva <79378029+eduardo-sdev@users.noreply.github.com>
2022-05-21 00:05:55 -0300

    aula 15 do while

---
+++ aulas/aula16-do-while/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int cont;
+
+    cont = 0;
+
+    do
+    {
+        cout << "Count = " << cont << "\n";
+        cont++;
+    } while (cont < 10);
+
+    cout << "\nFinish Task";
+
+    return 0;
+}
