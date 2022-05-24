f99cf602d9ffeedab7244455a1f43abae11892b0
Eduardo Silva <79378029+eduardo-sdev@users.noreply.github.com>
2022-05-24 10:09:09 -0300

    aula 18 array-vetor

---
+++ aulas/aula18-array-vetor/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int vetor[5] = {10,20,30,40,50};
+
+    /* vetor[0]=10; */
+    /* vetor[1]=20; */
+    /* vetor[2]=40; */
+    /* vetor[3]=50; */
+    /* vetor[4]=60; */
+
+    for(int i=0;i<sixeof(vetor)/4;i++){
+        cout << vetor[i] << "\n";
+    }
+
+    return 0;
+}
+
